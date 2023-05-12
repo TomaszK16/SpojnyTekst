063f88734f69d4a1f85e91f56ee48bf851a8e032
Tomasz Kośla <103423437+TomaszK16@users.noreply.github.com>
2023-05-12 19:31:29 +0200

    Add files via upload

---
+++ spojny fragment.py
@@ -0,0 +1,28 @@
+alphabet = [chr(number) for number in range(65,91)]
+
+rows = []
+while rows == []:
+    path = input("> Please enter the path to your file [.txt]: ")
+    if path.endswith(".txt") == False: path += ".txt"
+    try: rows = open(path).read().split("\n")
+    except: print("> Couldn't open the file! Please write the correct file path: ")
+    
+
+
+rows = [row for row in rows if row]
+
+for row in rows:
+    for character in row:
+        if character.upper() not in alphabet: row = row.replace(character, '')
+    splitRow = []
+    n = 0
+    for x in range(len(row)):
+        if x != 0 and row[x] != row[x-1]:
+            splitRow.append(str(row[n:x]))
+            n = x
+        if x == len(row) - 1: splitRow.append(str(row[n:]))
+        else: pass
+    longestString = max(splitRow, key=len)
+    print(longestString, len(longestString))
+
+input()
